e5dc631bbe02a8507828e5049e65fdd5ef5e3973
Ben Haugen <ben1232jazz@hotmail.com>
2019-01-24 02:14:10 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,10 @@
-my_collect(array)
-i = 0
- 
-  while i < array.length
-    yield array[i]
-    i = i + 1
-  end
-end 
+def my_collect(array)
+
+ 	i = 0
+	collection = []
+	while i < array.length
+    collection << yield(array[i])
+		i += 1
+	end
+	collection
+end
